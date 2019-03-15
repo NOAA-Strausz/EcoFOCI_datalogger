a756ec4d1c6e5212518e803c8850e69a1d1f1cb6
NOAA-Strausz <david.strausz@noaa.gov>
2019-03-15 12:05:42 -0700

    Add ability to poll instrument

--- foci_datalogger.py
+++ foci_datalogger.py
@@ -15,23 +15,25 @@ import serial
 #start instrument dictionary for keeping instrument info in
 instrument = {}
 
-instrument['gps'] = {'name':'gps', 'port':'/dev/ttyUSB0', 'baudrate':9600, 
+instrument = {'name':'gps', 'port':'/dev/ttyS0', 'baudrate':9600, 
        'bytesize':8, 'parity':'N', 'stopbits':1, 'timeout':1, 'xonxoff':0, 
-       'rtscts':0, 'dsrdtr':0}
+       'rtscts':0, 'dsrdtr':0, 'poll_string':'TS\r'}
 
 ser = serial.Serial()
-ser.port = instrument['gps']['port']
-ser.baudrate = instrument['gps']['baudrate']
-ser.timeout = instrument['gps']['timeout']
+ser.port = instrument['port']
+ser.baudrate = instrument['baudrate']
+ser.timeout = instrument['timeout']
 ser.open()
 
 file = open('test.txt','w')
 
 while True:
+    if instrument['poll_string']:
+        ser.write(str.encode(instrument['poll_string']))
     line=ser.readline()
     line=line.decode().strip()
     line=line+"\n"
-    if len(line) > 3:  
+    if len(line) > 5:  
         file.write(line)
         print(line,end='')
     file.flush()
